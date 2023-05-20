68c584b08efa827f4ecdbe57f5febb69e5879502
Nataliabskiba12 <nataliaskiba076@gmail.com>
2023-05-20 03:07:16 -0300

    push

---
+++ index.js
@@ -0,0 +1,27 @@
+var nombre = "Natalia";
+var apellido = "Skiba";
+console.log(nombre + " " + apellido);
+
+var num = 23;
+var numero = 45;
+var suma = num + numero;
+console.log(suma);
+
+var numerica = suma + 23;
+console.log(numerica);
+
+/*EJERCICIOS OPCIONALES*/
+var string = "hola";
+var number = 2;
+
+console.log(string + number);
+
+var number1 = 2;
+var booleano = true;
+
+console.log(number1 + booleano);
+
+var number2 = 10;
+var booleanoo = false;
+
+console.log(number2 + booleanoo);
